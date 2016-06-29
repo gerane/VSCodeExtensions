c8940cd4d9146eedca3f67bf6c4ac8d94912f32c
Brandon Padgett <gerane@gmail.com>
2016-06-29 13:47:19 -0500

    Added Command to Update Extensions
    This Command Checks all installed extensions for new versions and then installs them if outdated.

---
+++ VSCodeExtensions/Public/Update-VSCodeExtension.ps1
@@ -0,0 +1,62 @@
+function Update-VSCodeExtension
+{
+    [CmdletBinding(DefaultParameterSetName="ExtensionName",SupportsShouldProcess=$true)]
+    param
+    (
+        [switch]$Insiders                    
+    )
+    
+    Begin {}
+    
+    Process
+    {       
+        $InstalledExtensions = Get-VSCodeExtension $Insiders
+        $GalleryExtensions = Find-VSCodeExtension -ExtensionName '*' -WildCard
+        $ExtensionGroups = $InstalledExtensions | Group-Object -Property 'FullName'
+
+        foreach ($Group in $ExtensionGroups)
+        {
+            if ($Group.Count -gt '1')
+            {
+                Write-Debug -Message "Filtering out old Versions"
+                $Extension = ($Group.Group | Sort-Object -Descending -Property Version)[0]
+            }
+            else
+            {
+                $Extension = $Group.Group
+            }
+            
+            $LatestVersion = $GalleryExtensions | Where-Object { $_.FullName -eq $Extension.FullName }
+
+            if ($LatestVersion)
+            {
+                If ([Version]$Extension.Version -lt [Version]$LatestVersion.Version)
+                {
+                    Write-Verbose -Message "Extension $($Extension.FullName) with Version $($Extension.Version) needs to be updated"
+                    Write-Verbose -Message "New Version $($LatestVersion.Version) available."
+
+                    if ($PSCmdlet.ShouldProcess($($Extension.FullName), 'Update Extension'))
+                    {
+                        Try
+                        {
+                            Write-Verbose -Message "Installing Extension Update $($Extension.FullName)"
+                            Install-VSCodeExtension -FullName $Extension.FullName -Insiders:$Insiders
+                        }
+                        catch
+                        {
+                            Throw
+                        }
+                    }
+                }
+                else
+                {
+                    Write-Verbose -Message "No Updates available for Extension $($Extension.FullName)"
+                }
+            }
+            else
+            {
+                Write-Warning -Message "Extension $($Extension.FullName) was not found in the Gallery"
+            }
+        }            
+    }
+}
